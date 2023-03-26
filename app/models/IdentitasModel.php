ce27339b66b8a37c92f8dcde645126d75df8f07a
SoviaWahyuningtyas <116929784+SoviaWahyuningtyas@users.noreply.github.com>
2023-03-26 12:51:44 +0700

    ubah

--- app/models/IdentitasModel.php
+++ app/models/IdentitasModel.php
@@ -2,35 +2,44 @@
 
 // use Db as DB;
 
-class IdentitasModel extends Db {
+class IdentitasModel extends Db
+ {
     public $table = 'identitas';
     public $primaryKey = 'id';
 
-
-    public function getAll() {
+    public function getAll()
+ {
         return $this->get();
     }
 
-    public function getDb() {
+    public function getDb()
+ {
         return DB_NAME;
     }
 
-    public function getIdentitas($id) {
-        return $this->where('id',$id)->first();
-    }
-    
-    public function insertIdentitas($data) {
-        unset($data['submit']);
-        return $this->insertGetId($data);
+    public function getIdentitas( $id )
+ {
+        return $this->where( 'id', $id )->first();
     }
 
-    public function updateIdentitas($data, $id) {
-        unset($data['submit']);
-       return  $this->where($id)->update($data);
+    public function insertIdentitas( $data )
+ {
+        unset( $data[ 'submit' ] );
+
+        return $this->insertGetId( $data );
     }
 
-    public function deleteIdentitas($id) {
-        return $this->where($id)->delete();
+    public function updateIdentitas( $dat )
+ {
+        unset( $data[ 'submit' ] );
+        $id = $data[ 'id' ];
+        unset( $data[ 'id' ] );
+
+        return $this->where( $id )->update( $data );
     }
 
+    public function deleteIdentitas( $id )
+ {
+        return $this->where( $id )->delete();
+    }
 }
