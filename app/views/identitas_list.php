d14a976685416adcb0929d53fb4d36455b13cfa8
SoviaWahyuningtyas <116929784+SoviaWahyuningtyas@users.noreply.github.com>
2023-04-03 08:41:45 +0700

    ubah & identitas list

--- app/views/identitas_list.php
+++ app/views/identitas_list.php
@@ -85,8 +85,8 @@ require_once 'template/header.php';
             </div>
             <div class="modal-body">
 
-                <form action="<?=base_url() . 'identitas/insert';?>" method='post' enctype='multipart/form-data'>
-                    <table class=" table">
+                <form action="<?=base_url(). 'identitas/insert';?>" method="post" enctype="multipart/form-data">
+                    <table class="table">
                         <tr>
                             <td><button type="button" class="btn btn-success">NAMA</button></td>
                             <td>: </td>
