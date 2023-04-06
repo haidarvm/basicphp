6f6c73023a7128b638b1710d58f15bbc58031927
SoviaWahyuningtyas <116929784+SoviaWahyuningtyas@users.noreply.github.com>
2023-04-07 04:31:54 +0700

    ubah php

--- app/views/identitas_list.php
+++ app/views/identitas_list.php
@@ -1,17 +1,15 @@
 <?php
 if(!empty($alert)){
-    if($id) {
     echo '<script language="javascript">';
         echo "alert('Database Berhasil Ditambahkan!')";
-        "document.location.href = '" . base_url() . "'identitas/insert'";
+        "document.location.href = 'identitas_list.php" . base_url() . "'identitas/insert'";
         echo '</script>';
     }else{
     echo '<script language="javascript">';
         echo "alert('Database Berhasil Diubah!')";
-        "document.location.href = '" . base_url() . "'identitas/update'";
+        "document.location.href = 'ubah.php'" . base_url() . "'identitas/update'";
         echo '</script>'; 
     }
-}
 function css()
 {
 ?>
